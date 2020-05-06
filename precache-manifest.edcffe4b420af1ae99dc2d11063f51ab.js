d2a338b701ab292844efe27d73707f519d3cd747
Idan Pollak <idanpol1@gmail.com>
2020-05-06 19:32:25 +0300

    Updates

---
+++ precache-manifest.edcffe4b420af1ae99dc2d11063f51ab.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01e8b9b0b71eb7d58160d2012b517f60",
+    "url": "/Movies-App/index.html"
+  },
+  {
+    "revision": "d1d1cc7d43b9075ed2f2",
+    "url": "/Movies-App/static/js/2.9a787678.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Movies-App/static/js/2.9a787678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265de6b0e090a0e5fa31",
+    "url": "/Movies-App/static/js/main.bdcb6919.chunk.js"
+  },
+  {
+    "revision": "49bd89ce69fb3ce07524",
+    "url": "/Movies-App/static/js/runtime-main.c0622fee.js"
+  }
+]);
